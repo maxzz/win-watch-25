b32998f6f12c003eff888205b4cff5975717d0b0
Max Zakharzhevskiy <maxzz@msn.com>
2026-01-24 19:06:27 -0800

    WIP. esm format. 3. ok

--- electron/electron.vite.config.ts
+++ electron/electron.vite.config.ts
@@ -4,8 +4,24 @@ import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  main: {},
-  preload: {},
+  main: {
+    build: {
+      rollupOptions: {
+        output: {
+          format: 'es'
+        }
+      }
+    }
+  },
+  preload: {
+    build: {
+      rollupOptions: {
+        output: {
+          format: 'es'
+        }
+      }
+    }
+  },
   renderer: {
     resolve: {
       alias: {
