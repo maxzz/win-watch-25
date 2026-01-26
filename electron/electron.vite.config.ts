4b1123f7c4d3699f6bc780da8f255f1916a3c57a
Max Zakharzhevskiy <maxzz@msn.com>
2026-01-25 19:04:53 -0800

    WIP. add debug launch file

--- electron/electron.vite.config.ts
+++ electron/electron.vite.config.ts
@@ -1,33 +1,36 @@
-import { defineConfig } from 'electron-vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
+import { resolve } from 'path';
+import { defineConfig } from 'electron-vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  main: {
-    build: {
-      rollupOptions: {
-        output: {
-          format: 'es'
+    main: {
+        build: {
+            rollupOptions: {
+                output: {
+                    format: 'es'
+                }
+            }
         }
-      }
-    }
-  },
-  preload: {
-    build: {
-      rollupOptions: {
-        output: {
-          format: 'es'
-        }
-      }
-    }
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
     },
-    plugins: [react(), tailwindcss()]
-  }
-})
+    preload: {
+        build: {
+            rollupOptions: {
+                output: {
+                    format: 'es'
+                }
+            }
+        }
+    },
+    renderer: {
+        resolve: {
+            alias: {
+                '@renderer': resolve('src/renderer/src')
+            }
+        },
+        plugins: [
+            react(),
+            tailwindcss(),
+        ]
+    }
+});
