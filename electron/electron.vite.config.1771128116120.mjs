505fb712413fe56e1558a2dd394a0ca9f34a868f
Max Zakharzhevskiy <maxzz@msn.com>
2026-02-14 20:04:53 -0800

    WIP. someone created electron.vite.config

---
+++ electron/electron.vite.config.1771128116120.mjs
@@ -0,0 +1,44 @@
+// electron.vite.config.ts
+import { resolve } from "path";
+import { defineConfig } from "electron-vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+var electron_vite_config_default = defineConfig({
+    main: {
+        build: {
+            rollupOptions: {
+                output: {
+                    format: "es"
+                }
+            }
+        }
+    },
+    preload: {
+        build: {
+            rollupOptions: {
+                output: {
+                    format: "es"
+                }
+            }
+        }
+    },
+    renderer: {
+        resolve: {
+            alias: {
+                "@renderer": resolve("src/renderer/src")
+                // no need if run 'p i' from root directory, but interesting ways around, keep it for now
+                // Ensure single React instance
+                // 'react': resolve('node_modules/react'),
+                // 'react-dom': resolve('node_modules/react-dom'),
+            }
+            // dedupe: ['react', 'react-dom', 'jotai', 'valtio']
+        },
+        plugins: [
+            react(),
+            tailwindcss()
+        ]
+    }
+});
+export {
+    electron_vite_config_default as default
+};
