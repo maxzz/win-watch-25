7dca3f36dc76514a93bb3b416293f1793553aa10
Max Zakharzhevskiy <maxzz@msn.com>
2026-02-16 18:39:17 -0800

    WIP. new-icons. add SvgSymbolItem

--- electron/src/renderer/src/utils/util-hooks/spy-icons/2-test-all-svg-symbols.tsx
+++ electron/src/renderer/src/utils/util-hooks/spy-icons/2-test-all-svg-symbols.tsx
@@ -43,25 +43,29 @@ export function SpyTestAllSvgSymbols({ fontID = "svgfont", idPrefix, className, 
 
                         <div className="grid grid-cols-[repeat(auto-fill,minmax(0,64px))] gap-2">
                             {groupSymbols.map(
-                                ({ id, viewBox }) => (
-                                    <div key={id}>
-                                        <div className="size-16 1bg-[#6c7a6a] border-gray-700 border-4 rounded">
-                                            <svg viewBox={viewBox ?? "0 0 24 24"} className="w-full h-full fill-[#deb8f7] stroke-black stroke-[.5]">
-                                                <title>{`${id}`}</title>
-                                                <use xlinkHref={`#${id}`} />
-                                            </svg>
-                                        </div>
-
-                                        <div className="min-h-8 text-[.65rem] text-foreground text-center">
-                                            {id}
-                                        </div>
-                                    </div>
-                                )
+                                ({ id, viewBox }) => <SvgSymbolItem key={id} id={id} viewBox={viewBox} />
                             )}
                         </div>
                     </div>
                 )
             )}
+        </div>
+    );
+}
+
+function SvgSymbolItem({ id, viewBox }: Pick<SymbolItem, "id" | "viewBox">) {
+    return (
+        <div>
+            <div className="size-16 1bg-[#6c7a6a] border-gray-700 border-4 rounded">
+                <svg viewBox={viewBox ?? "0 0 24 24"} className="w-full h-full fill-[#deb8f7] stroke-black stroke-[.5]">
+                    <title>{`${id}`}</title>
+                    <use xlinkHref={`#${id}`} />
+                </svg>
+            </div>
+
+            <div className="min-h-8 text-[.65rem] text-foreground text-center">
+                {id}
+            </div>
         </div>
     );
 }
