6528e4bb0e712f93c890a5931480de07074dacff
Max Zakharzhevskiy <maxzz@msn.com>
2026-02-17 20:46:09 -0800

    WIP. new-icons. tweak tab and tabitem

--- electron/src/renderer/src/components/ui/icons/symbols/controls/6-control-tabitem.tsx
+++ electron/src/renderer/src/components/ui/icons/symbols/controls/6-control-tabitem.tsx
@@ -4,7 +4,7 @@ import { classNames } from "@renderer/utils";
 export function SvgSymbolControlTabItem() {
     return (
         <symbol id="control-tabitem" viewBox="0 0 24 24">
-            {/* header tabs */}
+            {/* header tabs inactive */}
             <path
                 d="M15.8,6.37v2.69h-3.99v-2.69c0-1.37-1.11-2.47-2.47-2.47h3.98c1.37,0,2.48,1.1,2.48,2.47Z"
                 fill="none"
@@ -13,6 +13,7 @@ export function SvgSymbolControlTabItem() {
                 strokeMiterlimit="10"
                 opacity="0.65"
             />
+            {/* header tabs active */}
             <path
                 d="M11.81,6.37v2.69H2.7v-2.69c0-1.37,1.11-2.47,2.48-2.47h4.16c1.36,0,2.47,1.1,2.47,2.47Z"
                 fill="none"
@@ -26,7 +27,7 @@ export function SvgSymbolControlTabItem() {
             <path
                 d="M2.71,9.06h16.74c1.02,0,1.85.83,1.85,1.85v7.34c0,1.02-.83,1.85-1.85,1.85H4.56c-1.02,0-1.85-.83-1.85-1.85v-9.19h0Z"
                 fill="currentColor"
-                opacity="0.12"
+                opacity="0.3"
                 stroke="currentColor"
                 strokeWidth="1.5"
                 strokeMiterlimit="10"
