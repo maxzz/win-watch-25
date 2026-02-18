78a0d8baf183ab8685dce271a0a843c769c4d566
Max Zakharzhevskiy <maxzz@msn.com>
2026-02-17 20:15:35 -0800

    WIP. new-icons. fix button and pane

--- electron/src/renderer/src/components/ui/icons/symbols/controls/b-control-pane.tsx
+++ electron/src/renderer/src/components/ui/icons/symbols/controls/b-control-pane.tsx
@@ -4,6 +4,7 @@ import { classNames } from "@renderer/utils";
 export function SvgSymbolControlPane() {
     return (
         <symbol id="control-pane" viewBox="0 0 24 24">
+            {/* background */}
             <rect
                 x="2.58"
                 y="2.58"
@@ -12,27 +13,29 @@ export function SvgSymbolControlPane() {
                 rx="3.18"
                 ry="3.18"
                 fill="currentColor"
-                opacity="0.10"
+                opacity="0.30"
                 stroke="currentColor"
                 strokeWidth="1.5"
                 strokeMiterlimit="10"
             />
+            {/* lower edge */}
             <path
                 d="M18.09,6.07v11.15c0,.6-.48,1.08-1.08,1.08H5.86"
                 fill="none"
-                stroke="currentColor"
+                stroke="white"
                 strokeWidth="1.5"
                 strokeMiterlimit="10"
                 strokeLinecap="square"
-                opacity="0.25"
+                opacity="0.7"
             />
+            {/* upper edge */}
             <path
                 d="M5.86,18.3V7.15c0-.6.48-1.08,1.08-1.08h11.15"
                 fill="none"
                 stroke="currentColor"
                 strokeWidth="1.5"
                 strokeMiterlimit="10"
-                opacity="0.85"
+                opacity="0.5"
             />
         </symbol>
     );
