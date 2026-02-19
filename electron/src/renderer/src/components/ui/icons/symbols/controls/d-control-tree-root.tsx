cea6a52c4db736af82ef5ce0437dec31609d6142
Max Zakharzhevskiy <maxzz@msn.com>
2026-02-18 21:05:30 -0800

    WIP. new-icons. tree components. 2. tweaks

--- electron/src/renderer/src/components/ui/icons/symbols/controls/d-control-tree-root.tsx
+++ electron/src/renderer/src/components/ui/icons/symbols/controls/d-control-tree-root.tsx
@@ -15,23 +15,18 @@ export function SvgSymbolControlTreeRoot() {
                     stroke: #000;
                     stroke-miterlimit: 10;
                   }
-
-                  .cls-3 {
-                    fill: #b3b3b3;
-                  }
             `}</style>
               </defs>
               <g id="Renders">
-                <rect className="cls-3" width="24" height="24"/>
                 <g id="tree_outline_parent" data-name="tree outline parent">
                   <path
                       id="to-chilld2" className="cls-1"
                       d="M14.15,19.61h-5.98c-.78,0-1.41-.68-1.41-1.51V7.62"
-                  />
-                  <line id="to-child1" className="cls-1" x1="7.25" y1="11.25" x2="14.15" y2="11.25"/>
-                  <rect id="child2" className="cls-1" x="14.62" y="16.87" width="5.25" height="5.25" rx="1.11" ry="1.11"/>
-                  <rect id="child1" className="cls-1" x="14.62" y="8.63" width="5.25" height="5.25" rx="1.11" ry="1.11"/>
-                  <rect id="root" className="cls-2" x="4.12" y="1.88" width="5.25" height="5.25" rx="1.11" ry="1.11"/>
+                  /> {/* to-chilld2 */}
+                  <line id="to-child1" className="cls-1" x1="7.25" y1="11.25" x2="14.15" y2="11.25"/> {/* to-child1 */}
+                  <rect id="child2" className="cls-1" x="14.62" y="16.87" width="5.25" height="5.25" rx="1.11" ry="1.11"/> {/* child2 */}
+                  <rect id="child1" className="cls-1" x="14.62" y="8.63" width="5.25" height="5.25" rx="1.11" ry="1.11"/> {/* child1 */}
+                  <rect id="root" className="cls-2" x="4.12" y="1.88" width="5.25" height="5.25" rx="1.11" ry="1.11"/> {/* root */}
                 </g>
               </g>
         </symbol>
