7a1b265d780b12da3de8c40027f6b651e3290bcc
Max Zakharzhevskiy <maxzz@msn.com>
2026-02-22 20:43:13 -0800

    WIP. ControlTreeNode. node rendered text. 4

--- electron/src/renderer/src/components/2-main/2-control-tree.tsx
+++ electron/src/renderer/src/components/2-main/2-control-tree.tsx
@@ -154,7 +154,13 @@ function getNodeText(node: ControlNode): string {
         return node.className ? node.className : typeName;
     }
     if (typeName === "Group") {
-        return node.className ? `${typeName}: ${node.className}` : typeName;
+        return (
+            node.className
+                ? `${typeName}: ${node.className}`
+                : node.name
+                    ? `${typeName}: "${node.name}"`
+                    : typeName
+        );
     }
     // if (typeName === "Text") {
     //     return node.name ? `Text: "${node.name}"` : typeName;
